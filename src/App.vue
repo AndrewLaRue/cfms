c4617064d84bcc6bd948268fa99caba0dd360635
andrewlarue <pikey6957@gmail.com>
2022-11-01 00:25:49 -0600

    cfms

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="container-fluid">
+    <div class="row align-items-center" title="Home">
+      <img class="col-3 py-2 img-fluid" src="src\assets\img\cfms_logo.png" alt="">
+      <h1 class="col-9">CFMS -<br> California Federation of Mineralogical Societies
+        <br>
+        <span class="fs-4">California Lapidary and Rockhounding Clubs</span>
+      </h1>
+    </div>
+  </header>
+  <main class="container-fluid">
+    <router-view />
+  </main>
+  <footer class=" container-fluid text-center bg-dark">
+    <div class="row">
+      <p class="text-light col-12 mb-1">CFMS - California Federation of Mineralogical Societies <a
+          href="https://cfmsinc.org/privacy-policy/">Privacy
+          Policy</a></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,191 @@
+<template>
+  <div id="background-tan-stone" class="row justify-content-around ">
+
+    <!-- Links -->
+    <section class="col-3 p-3">
+      <div class="card links-card">
+        <!-- <h2 class="card-title text-center">Links</h2> -->
+        <ul>
+          <li> <a href="https://cfmsinc.org/cfms-member-club-list/"> Member Clubs List</a></li>
+          <li> <a href="https://cfmsinc.org/shows-2/"> Shows</a></li>
+          <li> <a href="https://cfmsinc.org/newsletters/"> Newsletters</a></li>
+          <li> <a href="https://cfmsinc.org/cfms-history/"> History</a></li>
+          <li> <a href="https://cfmsinc.org/insurance-forms/"> Insurance Forms</a></li>
+          <li> <a href="https://cfmsinc.org/2020-cfms-show-forms/"> Show Forms</a></li>
+          <li> <a href="https://cfmsinc.org/forms/"> Forms and CFMS Documents</a></li>
+          <li> <a href="https://cfmsinc.org/roster/"> Roster (Password Protected)</a></li>
+          <li> <a href="https://cfmsinc.org/cfms-bylaws/"> Bylaws (Password Protected)</a></li>
+          <li> <a href="https://cfmsinc.org/speakers/"> Program Speakers available for Zoom meetings</a></li>
+          <li> <a href="https://cfmsinc.org/shows-2/submit-a-show/"> Submit a show</a></li>
+          <li> <a href="https://cfmsinc.org/cfms-bulletin-contest/"> Bulletin Contest</a></li>
+          <li> <a href="https://cfmsinc.org/rockhound-of-the-year/"> Rockhound Of The Year</a></li>
+          <li> <a href="https://cfmsinc.org/camp-zzyzx/"> Camp Zzyzx</a></li>
+          <li> <a href="https://cfmsinc.org/camp-paradise/"> Camp Paradise</a></li>
+          <li> <a href="https://cfmsinc.org/?page_id=411/"> Website Contest</a></li>
+          <li> <a href="https://cfmsinc.org/cfms-field-trips-3/"> Field Trips (Password Protected)</a></li>
+          <li> <a href="https://cfmsinc.org/fra-youth-home/"> FRA / Youth Home</a></li>
+          <ul>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/join-the-fra/"> Join the FRA</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/starting-a-youth-program/"> Starting a Youth Program</a>
+            </li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/fra-badge-program/"> FRA Badge Program</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/resources-for-youth-materials/"> Resources for Youth
+                Materials</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/kids-show-activities/"> Kids Show Activities</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/organization-of-youth-programs/"> Organization of Youth
+                Programs</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/fra-rock-stars/"> FRA Rock Stars</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/fra-honor-roll/"> FRA Honor Roll</a></li>
+            <li> <a href="https://cfmsinc.org/fra-youth-home/juniors-achievement-award/"> Juniors Achievement
+                Award</a>
+            </li>
+          </ul>
+          <li> <a href="https://cfmsinc.org/chairmans-manual/"> Chairman's Manual</a></li>
+          <li> <a href="https://cfmsinc.org/opregs/"> Opregs</a></li>
+          <li> <a href="https://cfmsinc.org/year-2020-contest-forms/"> All-American & Website contest forms</a></li>
+          <li> <a href="https://cfmsinc.org/program-media-dvds-ect/"> Visual Programs</a></li>
+          <li> <a href="https://cfmsinc.org/program-aides/"> Program Aides</a></li>
+        </ul>
+      </div>
+    </section>
+    <!-- Unique page components -->
+    <section class="col-9 img-text">
+      <h2 class="py-2">CFMS Home</h2>
+      <p>Welcome to the California Federation of Mineralogical Societies website. As a 501(3)(c) non-profit
+        organization we
+        represent Mineralogical and Lapidary clubs in California, Nevada, and Hawaii. We are affiliated with the
+        A.F.M.S. - The
+        American Federation of Mineralogical Societies
+
+        The objective and purpose of the Federation is to disseminate knowledge of mineralogy and the earth sciences
+        and to
+        encourage study in these subjects. To achieve that objective, we:</p>
+      <ul>
+        <li>
+          Represent associations of mineralogical societies, incorporated and unincorporated and their members as a
+          means of
+          encouraging further knowledge of geology, mineralogy, and rocks.
+        </li>
+        <li>
+          Provide exhibits with samples of rocks and minerals from various locations within California, Nevada, and
+          Hawaii.
+
+        </li>
+        <li>
+          Provide educational video programs that can be requested by societies to supplement their educational
+          monthly programs.
+
+        </li>
+        <li>
+          Provide a list of educational speakers on lapidary and mineralogical topics for societies to draw on to
+          provide programs
+          via Zoom or other presentation platforms.
+
+        </li>
+        <li>
+          Provide field trips for all societies to attend for exploration, study, and collection of specimens.
+
+        </li>
+        <li>
+          Provide a monthly newsletter to the societies with information about the ongoing activities and services
+          available for
+          member societies.
+
+        </li>
+        <li>
+          Maintain a scholarship program which provides financial assistance to qualified students majoring in the
+          earth
+          sciences
+          or lapidary and jewelry arts.
+
+        </li>
+        <li>
+          Provide classes in lapidary and jewelry arts.
+
+        </li>
+        <li>
+          Encourage fostering the interest of geology, mineralogy among young people.
+
+        </li>
+        <li>
+          Hold an annual gem and mineral show for the public to promote knowledge of rocks, gems, and minerals.
+
+        </li>
+        <li>
+          Support public museums with displays of minerals and lapidary arts.
+
+        </li>
+        <li>
+          Provide to the California Federation of Mineralogical Societies member societies a Public Liability
+          Insurance
+          program
+          for its members covering field trips, shows, meetings, etc.
+
+        </li>
+      </ul>
+
+      <br>
+
+      <p>Upcoming CFMS club shows and are listed in the Shows tab.</p>
+
+    </section>
+  </div>
+  <div id="background-stone" class="row">
+
+    <img class="col-8 img-fluid py-2"
+      src="https://i0.wp.com/cfmsinc.org/wp-content/uploads/2021/12/CFMS-2022-Executive-Committee.jpg?resize=768%2C614&ssl=1"
+      alt="">
+    <div class="col-4 img-text">
+      <div class="fs-3 text-center">
+
+        Our Officers for 2022!
+      </div>
+      <ul class="list-unstyled img-text">
+        <br>
+        Left to Right:
+        <li>
+          Jim Barton - President, President@CFMSinc.org
+        </li>
+        <li>
+          Bob Rush - 2nd Vice President, 2ndVP@CFMSinc.org
+        </li>
+        <li>
+          Kimberly Fiala - 1st Vice President, 1stVP@CFMSinc.org
+        </li>
+        <li>
+          Diane Cook - Treasurer, Treasurer@CFMSinc.org
+        </li>
+        <li>
+          Heidi Hall - Secretary, Secretary@CFMSinc.org
+        </li>
+
+
+
+      </ul>
+
+      (The current CFMS officers may be contacted by emailing the email addresses listed above)
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#background-stone {
+  background-image: url(https://as2.ftcdn.net/v2/jpg/03/10/36/01/1000_F_310360177_sMGdmtK0fItpByB2oJZSmzppBjQBSftA.jpg);
+  background-position: center;
+  background-size: cover;
+}
+
+#background-tan-stone {
+  background-image: url(https://production119files.stoneprofits.com/Files/Opustone/53972_9705_77_BM003.jpg?width=1024&autorotate=true);
+  background-position: center;
+  background-size: cover;
+}
+</style>
